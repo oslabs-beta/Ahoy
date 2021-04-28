e5574e74a0cc41554f8b48f70d64271ee2036b15
Yoko Ocarina <info@studio-libero.com>
2021-04-28 15:10:45 -0700

    Added table styling & App.css Co-authored-by: Jin Oh <65692508+ohjintech@users.noreply.github.com> Co-authored-by: Joe Bigelow <65265765+lilbigs2001@users.noreply.github.com> Co-authored-by: Tobey Forsman <116612+yebot@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import MainContainer from './containers/MainContainer';
-
-//import './assets/css/App.css';
 import 'semantic-ui-css/semantic.min.css'
+import './assets/css/App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Ahoy, mateys!</h1>
       <MainContainer />
-    </div>
   )
 }
 
