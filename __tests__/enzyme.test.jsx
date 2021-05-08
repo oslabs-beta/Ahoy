d4f2e445e92b8efdeb7f65ae4a737a0f68f933d5
Joe Bigelow <joebigelow@protonmail.com>
2021-05-08 17:29:50 -0500

    Tests rollback button icon

--- __tests__/enzyme.test.jsx
+++ __tests__/enzyme.test.jsx
@@ -11,25 +11,12 @@ import Version from '../src/components/Version';
 configure({ adapter: new Adapter() });
 
 describe('React unit tests', () => {
-  // let container = null;
-
-  // beforeEach(() => {
-  //   container = document.createElement('div');
-  //   document.body.appendChild(container);
-  // })
-
-  // afterEach(() => {
-  //   unmountComponentAtNode(container);
-  //   container.remove();
-  //   container = null;
-  // })
-
   describe('Version', () => {
     /* What do I want to test?
 
     ✓ Renders a table row
     ✓ Renders a single cell in the row
-    - Renders a list
+    ✓ Renders a list
     - Renders rollback icon button that runs the doHelmChartRollback function
     - Renders a list of 5 items
     - Renders content in each of those items
@@ -38,20 +25,16 @@ describe('React unit tests', () => {
 
     let wrapper;
 
-    const props = {
-      details: {
-        revision: 7,
-        updated: '2021-05-01T15:37:58.109638-05:00',
-        status: 'deployed',
-        chart: 'wordpress-10.10.1',
-        app_version: '5.7.0',
-        description: 'Upgrade complete',
-      },
-      release: 'chartVersion',
-      doHelmChartRollBack: jest.fn(),
+    const details = {
+      revision: 7,
+      updated: '2021-05-01T15:37:58.109638-05:00',
+      status: '',
+      chart: 'wordpress-10.10.1',
+      app_version: '5.7.0',
+      description: 'Upgrade complete',
     };
-
-    const { details, release, doHelmChartRollBack } = props;
+    const release = 'chartVersion';
+    const doHelmChartRollBack = jest.fn();
 
     beforeAll(() => {
       wrapper = shallow(<Version
@@ -72,6 +55,17 @@ describe('React unit tests', () => {
 
     it('Generates two lists', () => {
       expect(wrapper.find('[data-testid="list"]')).toHaveLength(2);
+    });
+
+    describe('Rollback icon', () => {
+      it('Renders a rollback icon if status does not equal "deployed"', () => {
+        expect(wrapper.find('[name="undo"]')).toHaveLength(1);
+      });
+
+      it('Calls function when clicked', () => {
+        wrapper.find('[name="undo"]').simulate('click');
+        expect(doHelmChartRollBack).toHaveBeenCalled();
+      });
     });
   });
 });
