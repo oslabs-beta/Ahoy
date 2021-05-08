bb441970081aaf1b5c55436cea3f507f3a6ecc08
Tobey Forsman <tobeyforsman@gmail.com>
2021-05-07 20:35:29 -0700

    Adjusts macOS Quit behavior. Some app name adjustments for build.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -42,7 +42,7 @@ module.exports = {
   },
   target: 'electron-renderer',
   plugins: [
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({ title: 'Ahoy!' }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
     })
