42f0f2bea0f9b04fc51c361b2cf3a3b3d19ff7b8
Tobey Forsman <tobeyforsman@gmail.com>
2021-05-08 19:06:55 -0700

    Initial jest tests for Helper functions

--- src/helpers/FileSystemHelper.js
+++ src/helpers/FileSystemHelper.js
@@ -1,44 +1,40 @@
 import yaml from 'yaml';
 import fs from 'fs';
 
-const path = require('path');
-
-const verifyLocalChartDir = function (localChartPath) {
+const verifyLocalChartDir = (localChartPath) => {
   if (!fs.existsSync(localChartPath)) {
-    console.log(`Local chart dir does not exist. Creating it. ${localChartPath}`);
-    fs.mkdirSync(localChartPath);
+    // console.log(`Local chart dir does not exist. Creating it. ${localChartPath}`);
+    return fs.mkdirSync(localChartPath);
   }
+  return true;
 };
 
-const getLocalCharts = function (path) {
-  return new Promise((resolve, reject) => {
-    // Verify charts folder
-    verifyLocalChartDir(path);
-    // read local charts folder, push to an array and return
-    const resultArray = [];
-    fs.readdir(path, (err, result) => {
-      if (err) {
-        console.error(`FileSystemHelper.js: ${err}`);
-      } else {
-        result.forEach((dir) => {
-          if (dir !== '.DS_Store') {
-            resultArray.push({ name: dir, version: 'yaml.version.placeholder' });
-          }
-        });
-        resolve(resultArray);
-      }
-    });
+const getLocalCharts = (path) => new Promise((resolve, reject) => {
+  // Verify charts folder
+  verifyLocalChartDir(path);
+  // read local charts folder, push to an array and return
+  const resultArray = [];
+  fs.readdir(path, (err, result) => {
+    if (err) {
+      console.error(`FileSystemHelper.js: ${err}`);
+    } else {
+      result.forEach((dir) => {
+        if (dir !== '.DS_Store') {
+          resultArray.push({ name: dir, version: 'yaml.version.placeholder' });
+        }
+      });
+      resolve(resultArray);
+    }
   });
-};
+});
 
-// This is work in progress.
-const getChartYAML = function (path, chartName) {
-  return new Promise((resolve, reject) => {
-    const file = fs.readFileSync(`${path}/${chartName}/Chart.yaml`, 'utf8');
-    resolve(yaml.parse(file));
-  });
-};
+// This is work in progress... to read a Chart.yaml and parse contents.
+const getChartYAML = (path, chartName) => new Promise((resolve, reject) => {
+  const file = fs.readFileSync(`${path}/${chartName}/Chart.yaml`, 'utf8');
+  resolve(yaml.parse(file));
+});
 
-export default {
+export {
+  verifyLocalChartDir,
   getLocalCharts,
 };
